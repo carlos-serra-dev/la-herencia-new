45f7b72f3f6f4c77200d07ebbef0b1214ea3d98a
Carlos Serra <carlos.serra.dev@gmail.com>
2025-06-09 11:49:37 +0200

    feat: add logo animation

--- src/components/event-logo.tsx
+++ src/components/event-logo.tsx
@@ -1,3 +1,13 @@
+import { motion } from "framer-motion";
+
 export default function EventLogo() {
-  return <img src="src/assets/images/alma-de-feria.png" />;
+  return (
+    <motion.img
+      src="src/assets/images/alma-de-feria.png"
+      initial={{ scale: 1.2 }}
+      animate={{ scale: 1 }}
+      transition={{ type: "spring", stiffness: 400, damping: 20}}
+      alt="ALMA DE FERIA"
+    />
+  );
 }
