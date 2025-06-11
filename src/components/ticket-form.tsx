16b176bee883009794a216a8954c04afd84068bb
Sergio <spm.dev.152@gmail.com>
2025-06-11 21:40:28 +0200

    fix: implement fixes #7

--- src/components/ticket-form.tsx
+++ src/components/ticket-form.tsx
@@ -1,10 +1,11 @@
-import { ArrowDownRight, CheckCircle } from "lucide-react";
+import { ArrowDownRight, ArrowLeft, CheckCircle } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import Button from "./ui/button";
 import TwoInitialLines from "./two-initial-lines";
 import EventLogo from "./event-logo";
 import EventDetails from "./event-details";
 import Partners from "./Partners";
+import { Link } from "react-router-dom";
 
 interface TicketFormProps {
   onBack: () => void;
@@ -121,17 +122,114 @@ export default function TicketForm({ onBack }: TicketFormProps) {
 
       <section ref={formRef} className="border-y-[1px] mt-8 py-8 px-6">
         <div className="max-w-4xl mx-auto">
-          <h1 className="text-3xl md:text-4xl font-bold text-red-600 mb-6 text-center">
+          <h1 className="text-3xl font-black tracking-widest font-stretch-condensed text-red-600 mb-6">
             ESTÁS A UN PASO DE CONSEGUIR TUS ENTRADAS.
           </h1>
 
-          <p className="text-lg mb-8 text-center text-red-600">
+          <p className="text-lg mb-8 text-red-600">
             Completa el siguiente formulario con tus datos. Una vez enviado,
             recibirás un correo con la información necesaria para poder realizar
             el pago y finalizar la compra.
           </p>
 
-          <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-6">
+          <form
+            onSubmit={handleSubmit}
+            className="hidden lg:block max-w-2xl mx-auto space-y-6"
+          >
+            <div className="flex flex-col">
+              <label className="block -mb-6 text-3xl font-black tracking-widest font-stretch-condensed text-red-600">
+                NOMBRE Y APELLIDOS
+              </label>
+
+              <input
+                type="text"
+                name="name_surname"
+                value={formData.name_surname}
+                onChange={handleInputChange}
+                required
+                className="w-11/12 self-end pl-80 border-b-2 border-red-600 bg-transparent pb-2 text-lg focus:outline-none focus:border-red-800"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="block -mb-6 text-3xl font-black tracking-widest font-stretch-condensed text-red-600">
+                EMAIL
+              </label>
+
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+                className="w-11/12 self-end pl-20 border-b-2 border-red-600 bg-transparent pb-2 text-lg focus:outline-none focus:border-red-800"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="block -mb-6 text-3xl font-black tracking-widest font-stretch-condensed text-red-600">
+                TELÉFONO
+              </label>
+
+              <input
+                type="tel"
+                name="phone"
+                value={formData.phone}
+                onChange={handleInputChange}
+                required
+                className="w-11/12 self-end pl-32 border-b-2 border-red-600 bg-transparent pb-2 text-lg focus:outline-none focus:border-red-800"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="block -mb-6 text-3xl font-black tracking-widest font-stretch-condensed text-red-600">
+                Nº ENTRADAS
+              </label>
+
+              <input
+                type="number"
+                name="tickets"
+                value={formData.tickets}
+                onChange={handleInputChange}
+                min="1"
+                max="w-11/12 self-end pl-20 border-b-2 border-red-600 bg-transparent pb-2 text-lg focus:outline-none focus:border-red-800"
+                required
+                pl-20
+                className="w-11/12 self-end pl-42 border-b-2 border-red-600 bg-transparent pb-2 text-lg focus:outline-none focus:border-red-800"
+              />
+            </div>
+
+            {error && (
+              <div className="p-4 bg-red-50 border border-red-200 rounded-lg">
+                <p className="text-red-600 font-bold">{error}</p>
+              </div>
+            )}
+
+            <div className="flex flex-col sm:flex-row items-center gap-4 pt-8">
+              <Link to={"/"}>
+                <Button
+                  type="submit"
+                  className="text-lg bg-transparent!"
+                  disabled={submitting}
+                >
+                  <ArrowLeft /> VOLVER
+                </Button>
+              </Link>
+
+              <Button type="submit" className="text-lg" disabled={submitting}>
+                {submitting ? "ENVIANDO..." : "ENVIAR"} <ArrowDownRight />
+              </Button>
+
+              <p className="text-xl font-bold text-red-600">
+                ¡TE ESPERAMOS EL 28 DE JUNIO!
+              </p>
+            </div>
+          </form>
+
+          <form
+            onSubmit={handleSubmit}
+            className="max-w-2xl block lg:hidden mx-auto space-y-6"
+          >
             <div>
               <label className="block text-xl font-bold text-red-600 mb-2">
                 NOMBRE Y APELLIDOS
@@ -200,7 +298,17 @@ export default function TicketForm({ onBack }: TicketFormProps) {
               </div>
             )}
 
-            <div className="flex flex-col sm:flex-row items-center gap-6 pt-8">
+            <div className="flex flex-col sm:flex-row items-center gap-4 pt-8">
+              <Link to={"/"}>
+                <Button
+                  type="submit"
+                  className="text-lg bg-transparent!"
+                  disabled={submitting}
+                >
+                  <ArrowLeft /> VOLVER
+                </Button>
+              </Link>
+
               <Button type="submit" className="text-lg" disabled={submitting}>
                 {submitting ? "ENVIANDO..." : "ENVIAR"} <ArrowDownRight />
               </Button>
